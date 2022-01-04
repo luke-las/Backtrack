d0ce2f8ebd4dbf5c8663636e94dd002930593106
luke-las <84208824+luke-las@users.noreply.github.com>
2022-01-04 11:37:19 -0500

    added js, site functional

--- JS/site.js
+++ JS/site.js
@@ -1,4 +1,21 @@
-//call hello
-function helloWorld(){
-    alert("hello world!");
+function getValues(){
+    let startValue = document.getElementById("startValue").value;
+    let stringReversed = reverseString(startValue);
+    displayString(stringReversed);
+}
+
+//reverse the string
+function reverseString(startValue){
+    let stringReversed = "";
+    for(i=startValue.length-1; i>=0; i--){
+        stringReversed += startValue[i];
+    }
+    return stringReversed;
+}
+
+//display the reversed string on the page
+function displayString(stringReversed){
+    
+    let templateRows = `<tr><td>${stringReversed}</tr></td>`
+    document.getElementById("results").innerHTML = templateRows;
 }
